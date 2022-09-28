405b4390881d5ddf8a12649b069f130d307f04f3
andreinalivaika <nav.testsw@gmail.com>
2022-09-28 12:20:25 +0300

    45 add react-redux lib, add connect for component container

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import style from './Profile.module.css'
 import {MyPostsContainer, ProfileInfo} from "../index"
 
-export default function Profile({ store }) {
+export default function Profile() {
     return (
         <main className={style.profileWrapper}>
             <ProfileInfo />
-            <MyPostsContainer store={store} />
+            <MyPostsContainer />
         </main>
     );
 }
