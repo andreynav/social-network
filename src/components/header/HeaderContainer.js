492641fde396e792b1962f5c105cd22ad1b7dddb
andreinalivaika <nav.testsw@gmail.com>
2022-10-08 10:30:20 +0300

    63 added DAL

--- src/components/header/HeaderContainer.js
+++ src/components/header/HeaderContainer.js
@@ -2,24 +2,16 @@ import React from "react";
 import {Header} from "../index";
 import {connect} from "react-redux";
 import {setAuthDataAC} from "../../store/authReducer";
-import axios from "axios";
+import {authAPI} from "../../api/api";
 
 class HeaderContainer extends React.Component {
-    authMe = () => {
-        return `https://social-network.samuraijs.com/api/1.0/auth/me`
-    }
-    
     componentDidMount() {
-        axios.get(this.authMe(), {
-            withCredentials: true
-        })
-            .then(response => {
-                if (response.data.resultCode === 1) {
-                    console.log(response.data.messages[0]);
+        authAPI.me()
+            .then(data => {
+                if (data.resultCode === 0) {
+                    this.props.setAuthDataAC({data: data.data});
                 }
-                this.props.setAuthDataAC({data: response.data.data});
-            })
-            .catch(error => console.log(error));
+            });
     }
 
     render() {
