36ebb776cb3ee715d1856d0bbe555eab53f48923
andreinalivaika <nav.testsw@gmail.com>
2022-11-18 17:55:06 +0300

    98 fill svg logo

---
+++ src/components/common/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import {ReactComponent as LogoSVG} from "../../../assets/img/logo.svg"
+
+export const Logo = () => {
+    return (
+        <LogoWrapper>
+            <LogoSVG/>
+        </LogoWrapper>
+    )
+}
+
+const LogoWrapper = styled.div`
+  & svg {
+    height: auto;
+    width: auto;
+  }
+  
+  & g {
+    fill: ${props => props.theme.colorSecondary};
+  }
+`
