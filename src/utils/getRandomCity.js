c196629236448f2a559e18d90ca4a25ced283628
andreinalivaika <nav.testsw@gmail.com>
2022-11-19 13:01:09 +0300

    99 move method getRandomCity to utils

---
+++ src/utils/getRandomCity.js
@@ -0,0 +1,26 @@
+const cities = [
+    'Minsk',
+    'New York',
+    'Paris',
+    'Berlin',
+    'London',
+    'Praga',
+    'Sidney',
+    'Vena',
+    'Brno',
+    'Vilnus',
+    'Dubai',
+    'Moscow',
+    'Kiev',
+    'Grodno',
+    'Vitebsk',
+    'Brest',
+    'Gomel',
+    'Las Vegas',
+    'Los Angeles'
+]
+
+
+export const getRandomCity = () => {
+    return cities[Math.floor(Math.random() * cities.length)]
+}
