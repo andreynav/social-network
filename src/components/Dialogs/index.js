15d92bd101a7f9d074b8b2f6ec4a067676e2302e
andreinalivaika <nav.testsw@gmail.com>
2022-12-02 18:11:26 +0300

    100 removed index.js from dialog folder

--- src/components/Dialogs/index.js
+++
@@ -1,9 +0,0 @@
-import Dialogs from "./Dialogs";
-import DialogUser from "./DialogUser";
-import DialogMessage from "./DialogMessage";
-
-export {
-    Dialogs,
-    DialogUser,
-    DialogMessage
-}
